ed22fcc68a81d0842053b600b97f6a0123836bde
ryain11 <mirsadakca55@gmail.com>
2026-09-08 17:17:29 +0300

    Clang formatting

--- src/fdosecrets/objects/Service.cpp
+++ src/fdosecrets/objects/Service.cpp
@@ -603,14 +603,6 @@ namespace FdoSecrets
 
     void Service::onDatabaseUnlockDialogFinished(bool accepted, DatabaseWidget* dbWidget)
     {
-        if (!dbWidget) {
-            if (m_unlockingAnyDatabase) {
-                emit doneUnlockDatabaseInDialog(false, dbWidget);
-                m_unlockingAnyDatabase = false;
-            }
-            return;
-        }
-
         if (!m_unlockingAnyDatabase && !m_unlockingDb.contains(dbWidget)) {
             // not our concern
             return;
